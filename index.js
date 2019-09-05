963e64dee4ebe0389dcdbc5966cd65f806e532af
Katie Davis <katie.davis@shopify.com>
2019-09-05 12:06:31 -0400

    simplify subcommand name

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
 #! /usr/bin/env node
 
 function receiveArgs(args) {
-  console.log(args)
   const type = args[2];
   switch (type) {
-    case "generate-empty-state-page": {
+    case "empty-state-page": {
       const generatePage = require("./page/generate-page");
       const createEmptyStateTemplate = require("./page/empty-state-template");
       generatePage(createEmptyStateTemplate, "pages", args);
       break;
     }
-    case "generate-two-column-page": {
+    case "two-column-page": {
       const generatePage = require("./page/generate-page");
       const createTwoColumnTemplate = require("./page/two-column-template");
       generatePage(createTwoColumnTemplate, "pages", args);
       break;
     }
-    case "generate-settings-page": {
+    case "settings-page": {
       const generatePage = require("./page/generate-page");
       const createSettingsTemplate = require("./page/settings-template");
       generatePage(createSettingsTemplate, "pages", args);
       break;
     }
-    case "generate-list-page": {
+    case "list-page": {
       const generatePage = require("./page/generate-page");
       const createListTemplate = require("./page/list-template");
       generatePage(createListTemplate, "pages", args);
       break;
     }
-    case "generate-recurring-billing": {
+    case "recurring-billing": {
       const generateRecurringBilling = require("./billing/generate-recurring-billing");
       const transform = require("./transform");
       transform("server/server.js", generateRecurringBilling);
       break;
     }
-    case "generate-one-time-billing": {
+    case "one-time-billing": {
       const generateOneTimeCharge = require("./billing/generate-one-time-charge");
       const transform = require("./transform");
       transform("server/server.js", generateOneTimeCharge);
       break;
     }
-    case "generate-webhook": {
+    case "webhook": {
       const type = args[3];
       const generateWebhook = require("./webhook/generate-webhook");
       const transform = require("./transform");
