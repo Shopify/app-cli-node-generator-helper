65ee35889b2f9766506b8313fef2ea248c5ae17c
Katie Davis <katie.davis@shopify.com>
2019-09-06 10:27:34 -0400

    remove string

--- tests/generate-page.test.js
+++ tests/generate-page.test.js
@@ -2,21 +2,6 @@ const fs = require("fs");
 const generatePage = require("../page/generate-page");
 const generateEmptyState = require("../page/empty-state-template");
 
-const content = `import { EmptyState, Page } from "@shopify/polaris";
-const Index = () => (
-  <Page>
-    <EmptyState
-      heading="Manage your inventory transfers"
-      action={{ content: 'Add transfer' }}
-      secondaryAction={{ content: 'Learn more about empty state', url: 'https://polaris.shopify.com/components/structure/empty-state' }}
-      image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
-    >
-      <p>Track and receive your incoming inventory from suppliers.</p>
-    </EmptyState>
-  </Page>
-)
-export default Index;`
-
 jest.mock("fs", () => ({
   writeFileSync: jest.fn(),
   existsSync: page => page === "pages/test.js" ? true : false
